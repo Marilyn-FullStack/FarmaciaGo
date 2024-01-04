f609e8a1dd59fb0441da7b798157b7ec1096e1b4
Maria <m.leiliane.lobo@gmail.com>
2024-01-04 13:26:15 -0300

    CRUD concluido

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,51 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 100)
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 7, max = 100, message = "O atributo nome deve conter no mínimo 7 e no máximo 100 caracteres")
+	private String descricao;
+
+	// Construtores, getters e setters
+	public Categoria() {
+	}
+
+	public Categoria(Long id,
+			@NotBlank(message = "O atributo nome é obrigatório!") @Size(min = 7, max = 100, message = "O atributo nome deve conter no mínimo 7 e no máximo 100 caracteres") String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
